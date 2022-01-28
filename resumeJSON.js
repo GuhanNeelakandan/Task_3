0e060afd94189f93f7e8280f00991c014fa66fbb
Guhan <you@example.com>
2022-01-28 17:31:25 +0530

    task 3

---
+++ resumeJSON.js
@@ -0,0 +1,52 @@
+let myResume={
+    "basics": {
+      "name": "GUHAN N S",
+      "email": "neelakandanguhan@gamil.com",
+      "phone": 9790850035,
+      "degree": "B.E",
+      "location": {
+        "address": "3/876 pilliayar kovil street mettukuppam ",
+        "postalCode": 600097,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+    "work": 
+      {
+        "company": "Hyundai Motor Plaza",
+        "position": "Service Advisor",
+        "startDate": "21/12/2020",
+        "endDate": "05/12/2021",
+        "summary": "i am the service advisor communicates with customers to determine their vehicular problems and the repair timeline and provides the technicians with accurate repair descriptions about the customers' concerns.",
+      },
+    },
+    "education": [
+      {
+        "institution": "Tagore engineering college",
+        "department": "Mechanical",
+        "studyType": "fulltime",
+        "batch start year": 2015,
+        "batch end year": 2019,
+        "gpa": 6.74,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript,photshop,solid work",
+        "level": "beginer",
+        "project":"design the automobile equipement in college"
+        
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "photoshop,drawing,",
+      }
+    ]
+  }
+  console.log(myResume);
